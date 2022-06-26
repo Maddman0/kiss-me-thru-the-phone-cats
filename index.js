b2db1e1b507f76ebdda23115cfa968ee794e6ab0
Maddman0 <maddman2001@gmail.com>
2022-06-25 19:33:38 -0700

    done

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 var audio = new Audio('/kiss_me_thru_the_phone_nightcore.mp3');
-window.onload = () => {audio.play()};
-    
+let playButton = document.getElementById("play");
+let startTime = 0;
+let img = new Image();
+let jumpTime = 0;
+document.body.appendChild(img);
+document.body.appendChild
 
-function display_image(index) {
-    path = '/audio/' + index + '.jpg';
-    let img = new Image();
-    img.src = 'audio/1.jpg';
-    document.body.appendChild(img);
+
+playButton.addEventListener("click", () => {
+    audio.play();
+    audio.currentTime = jumpTime;
+    startTime = performance.now();
+    playButton.remove();
+    init();
+});
+
+let transition_times = [4, 11, 13, 14.5, 15.5, 16.8, 18.2, 19.5, 22.8];
+
+
+function init () {
+    requestAnimationFrame(tick);
 }
 
-display_image(2);
+function tick() {
+    transition_times.forEach((transition,index) => {
+        if (getElapsedTime() > (transition - jumpTime * 1.0)) 
+        {
+            console.log("transitioned");
+            display_image(index+1);
+        } 
+    });
+    requestAnimationFrame(tick);
+}
+
+function display_image(index) {
+    path = '/images/' + index + '.jpg';
+    img.src = path;
+}
+
+function clear_body() {
+    document.body.innerHTML = '';
+}
+
+function getElapsedTime() {
+    return (performance.now() - startTime)/1000.0;
+
+}
 
 
 
